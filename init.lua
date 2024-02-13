908e2b2831b5e108fdb67d11c0d0f3c5b5fe19f4
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2024-02-13 15:04:12 +0100

    Add Obsidian vim

--- init.lua
+++ init.lua
@@ -299,6 +299,83 @@ require('lazy').setup({
     vim.keymap.set('n', '<Leader>tt', '<cmd>:ChatGPTEditWithInstructions<cr>'),
   },
 
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        --  {
+        --    name = "personal",
+        --    path = "~/vaults/personal",
+        --  },
+        {
+          name = "anfrydev",
+          path = "~/Obsidian/anfrydev",
+        },
+      },
+
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+        new_notes_location = "current_dir",
+        prepend_note_id = true,
+      },
+
+      mappings = {
+        -- Obsidian follow.
+        ["<leader>of"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes. Obsidian done
+        ["<leader>od"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+
+      -- Optional, alternatively you can customize the frontmatter data.
+      note_frontmatter_func = function(note)
+        -- This is equilavent to the default frontmatter function.
+        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+        return out
+      end,
+
+      templates = {
+        subdir = "Templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+        tags = "",
+      },
+    },
+  },
 
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
